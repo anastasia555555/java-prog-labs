3e22fb49a49bef345a18cbfca4f832205e0ab126
Anastasia Kaliuzhna <103441373+anastasia555555@users.noreply.github.com>
2023-10-25 13:32:26 +0300

    Create Main.java

---
+++ Lab1/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main
+{
+  public static void main(String[] args) {
+        // O1 = "+"
+        // C3 = 15
+        // O2 = "%"
+        // C7 = char
+        
+        Calculator calc = new Calculator('a', 'c', 15);
+        float result = calc.func_one('v', 'w');
+        System.out.println(result);
+
+  }
+}
